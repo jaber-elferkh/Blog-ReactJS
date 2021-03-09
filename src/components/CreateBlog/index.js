a60bd7a59cc0babbab72e9abcc0443a26769613e
jaeel29 <elferkhjaber@gmail.com>
2021-03-09 20:19:46 +0100

    useHistory

--- src/components/CreateBlog/index.js
+++ src/components/CreateBlog/index.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import './styles.scss';
 
 function CreateBlog() {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('ahmed');
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = { title, author, body };
 
-    console.log(blog);
+    setLoading(true);
+    setTimeout(() => {
+      fetch('http://localhost:4000/blogs/', {
+        method: 'POST',
+        headers: { 'content-Type': 'application/json' },
+        body: JSON.stringify(blog),
+      }).then(() => {
+        console.log('new blog added');
+        setLoading(false);
+        history.push('/');
+      });
+    }, 1000);
   };
 
   return (
@@ -40,7 +54,8 @@ function CreateBlog() {
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
 
-        <button>Add Blog</button>
+        {!loading && <button>Add Blog</button>}
+        {loading && <button disabled>Adding Blog ...</button>}
       </form>
     </div>
   );
